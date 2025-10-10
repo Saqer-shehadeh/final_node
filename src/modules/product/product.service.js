4968b22547eb771db4aad298f1750ee322b5d8bb
Saqer <saqershehadeh2@gmail.com>
2025-10-10 23:14:25 +0300

    default import

--- src/modules/product/product.service.js
+++ src/modules/product/product.service.js
@@ -5,7 +5,7 @@ import { getSubCategoryForProduct } from "../subcategory/subcategory.data.js"
 import { getPagingData } from "../../utils/pagination.js/pagination.js"
 import objectId from "../../validation/objectId.validator.js"
 import { getCategoryById } from "../category/category.data.js"
-import  deleteImageFile  from "../../utils/images/deleteImageFile.js"
+import deleteImageFile from "../../utils/images/deleteImageFile.js"
 
 
 
@@ -58,17 +58,17 @@ export const updateProduct = async (id, body) => {
         if (!body.category) {
             const subCategory = await getSubCategoryForProduct(product.category, body.subCategory)
             if (!subCategory) throw new AppError("invalid subCategory for this category", 400)
-        }else{
+        } else {
             const subCategory = await getSubCategoryForProduct(body.category, body.subCategory)
             if (!subCategory) throw new AppError("invalid subCategory for this category", 400)
         }
-    
+
     }
     if (body.price && body.discount) {
         body.finalPrice = body.price * (1 - body.discount / 100)
     } else if (body.price && !body.discount) {
         body.finalPrice = body.price * (1 - product.discount / 100)
-    }else if (!body.price && body.discount) {
+    } else if (!body.price && body.discount) {
         body.finalPrice = product.price * (1 - body.discount / 100)
     }
     const updated = await data.updateProduct(id, body)
@@ -77,16 +77,17 @@ export const updateProduct = async (id, body) => {
 
 export const deleteProduct = async (id) => {
     const validId = objectId(id)
-    if (!validId) throw new AppError("invalid id", 400) 
+    if (!validId) throw new AppError("invalid id", 400)
     const product = await data.getProductById(id)
     if (!product) throw new AppError("no product found", 404)
-        if(product.mainImage){
-            deleteImageFile(product.mainImage)
-        }
-        if(product.subImages && product.subImages.length >0){
-            product.subImages.forEach((image)=>{
-                deleteImageFile(image)
-            })}
+    if (product.mainImage) {
+        deleteImageFile(product.mainImage)
+    }
+    if (product.subImages && product.subImages.length > 0) {
+        product.subImages.forEach((image) => {
+            deleteImageFile(image)
+        })
+    }
 
 
     const deleted = await data.deleteProduct(id)
